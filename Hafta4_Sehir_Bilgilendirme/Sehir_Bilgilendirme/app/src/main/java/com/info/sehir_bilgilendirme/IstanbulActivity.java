f6e11c2116efe9156b9ad0b1abd0fa48d8adfb52
mustafanazli <mustafanazli1137@gmail.com>
2026-03-24 11:38:22 +0300

    Sehir Bilgilendirme Uygulaması

---
+++ Hafta4_Sehir_Bilgilendirme/Sehir_Bilgilendirme/app/src/main/java/com/info/sehir_bilgilendirme/IstanbulActivity.java
@@ -0,0 +1,17 @@
+package com.info.sehir_bilgilendirme;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class IstanbulActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_istanbul);
+    }
+}
